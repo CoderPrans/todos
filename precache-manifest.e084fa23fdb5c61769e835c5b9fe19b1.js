a71271362c5bcc735eec1827f4e1332ffb2f303a
CoderPrans <pranavbhaskar17@gmail.com>
2019-02-24 00:02:51 +0530

    Updates

---
+++ precache-manifest.e084fa23fdb5c61769e835c5b9fe19b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f33db40a541861ed028e",
+    "url": "/todos/static/js/runtime~main.f33db40a.js"
+  },
+  {
+    "revision": "ed4245e5b8ab174138fa",
+    "url": "/todos/static/js/main.ed4245e5.chunk.js"
+  },
+  {
+    "revision": "8704c04561cef13131e8",
+    "url": "/todos/static/js/2.8704c045.chunk.js"
+  },
+  {
+    "revision": "ed4245e5b8ab174138fa",
+    "url": "/todos/static/css/main.162bc577.chunk.css"
+  },
+  {
+    "revision": "32a060a4db56ba47acbdbc51a9983d8c",
+    "url": "/todos/index.html"
+  }
+];
