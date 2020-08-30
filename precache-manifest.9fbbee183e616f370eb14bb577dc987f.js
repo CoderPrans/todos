158647ad6f7796434f7ea24162f2807ffb95a256
Pranav Bhaskar <pranavbhaskar17@gmail.com>
2020-08-30 22:44:49 +0530

    Updates

---
+++ precache-manifest.9fbbee183e616f370eb14bb577dc987f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac5c99dda9da4fff6067777b0da697bc",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "13953eb7127a340a5d26",
+    "url": "/todos/static/css/main.fa8cd6e4.chunk.css"
+  },
+  {
+    "revision": "fae062c9883088a8d63d",
+    "url": "/todos/static/js/2.6ba88c9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos/static/js/2.6ba88c9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13953eb7127a340a5d26",
+    "url": "/todos/static/js/main.b370803f.chunk.js"
+  },
+  {
+    "revision": "06f352b65e540bf1c983",
+    "url": "/todos/static/js/runtime-main.c409f8d6.js"
+  }
+]);
